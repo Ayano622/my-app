acd60aaed282865b67e8dbf6e2e6d880f15dcd65
Ayano <takitaayano478@gmail.com>
2025-02-01 20:33:50 +0900

    add home screen

---
+++ src/app/components/Sidebar/index.tsx
@@ -0,0 +1,21 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+export const Sidebar = () => {
+  return (
+    <div className='sideBar'>
+        <Box>
+            AAAA
+        </Box>
+        <Box>
+            BBBB
+        </Box>
+        <Box>
+            CCCC
+        </Box>
+        <Box>
+            DDDD
+        </Box>   
+    </div>
+  )
+}
