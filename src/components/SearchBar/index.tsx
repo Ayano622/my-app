655a01ab42a354911e5fd8d6421771291153ea69
Ayano <takitaayano478@gmail.com>
2025-04-29 22:28:22 +0900

    fixed bugs and renamed menuItem components

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -5,14 +5,14 @@ import SearchIcon from '@mui/icons-material/Search';
 export const SearchBar = () => {
   return (
     <div style={{display: 'flex'}}>
-      <Paper sx={{ width: '200px', display: 'flex', boxShadow:'none', backgroundColor: "#f0f0f0"}} >
+      {/* <Paper sx={{ width: '200px', display: 'flex', boxShadow:'none', backgroundColor: "#f0f0f0"}} >
       <IconButton aria-label="menu">
         <SearchIcon>
         </SearchIcon>
         </IconButton>
         <InputBase sx={{width: '200px', marginLeft: '0px'}}>
           </InputBase>
-      </Paper>
+      </Paper> */}
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,25 @@
 import { Home } from "@/features/Home";
 import { Header } from "@/layout/Header";
 
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Header />
+//       <Home />
+//       </ div>
+//   );
+// }
+// import { CssVarsProvider } from '@mui/joy/styles'; // MUI Joyの場合
+// または
+// import { ThemeProvider, createTheme } from "@mui/material/styles"; // MUI Materialの場合
+
+// const theme = createTheme({})
+
 export default function HomePage() {
   return (
     <div>
       <Header />
       <Home />
-      </ div>
+    </div>
   );
 }
