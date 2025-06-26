685ee0a17f1b740c9ca5c67d2e88c9c0af1e68c9
Ayano <takitaayano478@gmail.com>
2025-06-26 10:00:47 +0900

    fix post page and add mylist page

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,29 +1,29 @@
-import { Box } from "@mui/material";
-import './index.module.css'
+import { Box, Button } from "@mui/material";
+import "./index.module.css";
+import { useRouter } from "next/navigation";
 
 export const Post = () => {
+  const router = useRouter();
+  const handleOnClick = () => {
+    router.push("/list");
+  };
   return (
     <>
-        <div className="post">
-            <Box sx={{width: '800px', height: '200px', color: 'gray'}}>
-                画像を入れる
-                画像を入れる
-                画像を入れる
-            </Box>
-            <Box sx={{width: '800px', height: '100px', color: 'gray'}}>
-                タイトル
-            </Box>
-            <Box sx={{width: '800px', height: '100px', color: 'gray'}}>
-                キャプション
-                キャプション
-                キャプション
-            </Box>
-            <Box sx={{width: '800px', height: '200px', color: 'gray'}}>
-                感想
-                感想
-                感想
-            </Box>
-        </div>
-        </>
+      <div className="post">
+        <Box sx={{ width: "800px", height: "200px", color: "gray" }}>
+          画像を入れる 画像を入れる 画像を入れる
+        </Box>
+        <Box sx={{ width: "800px", height: "100px", color: "gray" }}>
+          タイトル
+        </Box>
+        <Box sx={{ width: "800px", height: "100px", color: "gray" }}>
+          キャプション キャプション キャプション
+        </Box>
+        <Box sx={{ width: "800px", height: "200px", color: "gray" }}>
+          感想 感想 感想
+        </Box>
+        <Button onClick={handleOnClick}>投稿</Button>
+      </div>
+    </>
   );
 };
---
+++ src/pages/list/index.tsx
@@ -0,0 +1,16 @@
+import { Header } from '../../layout/Header'
+import React from 'react'
+
+export default function ListPage () {
+  return (
+    <div>
+        <Header />
+        <div>
+            my list my list my list my list<br></br>
+            my list my list my list my list<br></br>
+            my list my list my list my list<br></br>
+        </div>
+    </div>
+    
+  )
+}
